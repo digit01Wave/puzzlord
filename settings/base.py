a1bf3193180900d795d2e770aa2ab77c9c7d3473
Jessica Hoang <jessicth94@gmail.com>
2021-02-07 22:54:48 -0800

    bypass git 2

--- settings/base.py
+++ settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "9PZVMK3zEPpn8PE7")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
