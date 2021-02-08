77b4ab97a4e9f8374d34c90a5ecbdf7316a42c50
Jessica Hoang <jessicth94@gmail.com>
2021-02-07 16:44:12 -0800

    added all to allowed hosts

--- puzzlord/settings.py
+++ puzzlord/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = "9PZVMK3zEPpn8PE7"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
